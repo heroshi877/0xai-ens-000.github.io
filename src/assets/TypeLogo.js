95927a009d2c04d03d4fbade784f51ef19f1778b
heroshi877 <107963914+heroshi877@users.noreply.github.com>
2023-03-01 19:18:16 +0100

    Update TypeLogo.js

--- src/assets/TypeLogo.js
+++ src/assets/TypeLogo.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import styled from '@emotion/styled/macro'
 
-
+const SVG = styled('svg')`
+  width: 57px;
+  height: 22px;
+  margin-left: 10px;
+  g {
+    fill: ${p => (p.color ? p.color : 'transparent')};
+  }
+`
 
 const TypeLogo = ({ color }) => (
   <SVG color={color} width="57" height="22" xmlns="http://www.w3.org/2000/svg">
