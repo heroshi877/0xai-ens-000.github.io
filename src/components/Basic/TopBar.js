605d7453e7e37ee25314bd06e8af583ed1ec7cf2
heroshi877 <107963914+heroshi877@users.noreply.github.com>
2023-03-02 03:30:00 +0100

    Update TopBar.js

--- src/components/Basic/TopBar.js
+++ src/components/Basic/TopBar.js
@@ -9,7 +9,7 @@ const TopBar = styled('div')`
   border-bottom: 1px solid #ededed;
   box-shadow: 0 2px 4px 0 rgba(181, 177, 177, 0.2);
 
-  background: #5a5a5a;
+  background: #323232;
 
   ${mq.small`
     padding: 20px 40px;
