8a36840e5fabfb8f1c1481477274311644506788
heroshi877 <107963914+heroshi877@users.noreply.github.com>
2023-03-03 20:26:48 +0100

    Update Explainer.js

--- src/components/SingleName/DNSNameRegister/Explainer.js
+++ src/components/SingleName/DNSNameRegister/Explainer.js
@@ -1,25 +1,131 @@
 import React from 'react'
 import styled from '@emotion/styled/macro'
+import { useTranslation } from 'react-i18next'
+
+import mq from 'mediaQuery'
 import Step from './Step'
+import Button from '../../Forms/Button'
+import { ReactComponent as Bell } from '../../Icons/Bell.svg'
+import { ReactComponent as Tick } from '../../Icons/GreyCircleTick.svg'
+
+import { requestPermission, hasPermission } from './notification'
 
 const Steps = styled('section')`
   display: grid;
   grid-template-columns: 1fr;
   grid-row-gap: 30px;
-  border-bottom: ${p => (p.border ? '1' : '0')}px solid #dfdfdf;
+  ${mq.large`
+    grid-template-columns: repeat(3, 1fr);
+    grid-column-gap: 30px;
+    grid-template-rows: 1fr;
+  `}
 `
 
-const Explainer = ({ step, number, title, text, border }) => {
+const Header = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+
+  h2 {
+    font-family: Overpass;
+    font-weight: 300;
+    font-size: 18px;
+    color: #745600;
+    letter-spacing: 0;
+    margin: 0;
+    margin-bottom: 5px;
+
+    ${mq.medium`
+      font-size: 24px;
+      color: #745600;
+      letter-spacing: 0;
+    `}
+  }
+  
+  p {
+    margin: 0;
+    font-weight: 400;
+    font-family: Overpass;
+    font-size: 14px;
+    color: #f3ba2f;
+    letter-spacing: 0;
+  }
+`
+
+const NotifyButton = styled(Button)`
+  flex-shrink: 0;
+`
+
+const NotifyButtonDisabled = styled('div')`
+  color: hsla(0, 0%, 82%, 1);
+`
+
+const Explainer = ({ step, waitPercentComplete, waitTime }) => {
+  const { t } = useTranslation()
+  const titles = {
+    PRICE_DECISION: t('register.titles.0'),
+    COMMIT_SENT: t('register.titles.1'),
+    COMMIT_CONFIRMED: t('register.titles.1'),
+    AWAITING_REGISTER: t('register.titles.1'),
+    REVEAL_SENT: t('register.titles.1'),
+    REVEAL_CONFIRMED: t('register.titles.2')
+  }
+
   return (
     <>
-      <Steps border={border}>
+      <Header>
+        <div>
+          <h2>{titles[step]}</h2>
+          <p>{t('register.favourite')}</p>
+        </div>
+        {hasPermission() ? (
+          <NotifyButtonDisabled>
+            <Tick style={{ marginRight: 5 }} />
+            {t('register.notify')}
+          </NotifyButtonDisabled>
+        ) : (
+          <NotifyButton type="hollow-primary" onClick={requestPermission}>
+            <Bell style={{ marginRight: 5 }} />
+            {t('register.notify')}
+          </NotifyButton>
+        )}
+      </Header>
+
+      <Steps>
         <Step
-          number={number}
+          number={1}
           progress={
-            step === 'SUBMIT_CONFIRMED' ? 100 : step === 'SUBMIT_SENT' ? 50 : 0
+            step === 'PRICE_DECISION' ? 0 : step === 'COMMIT_SENT' ? 50 : 100
           }
-          title={title}
-          text={text}
+          title={t('register.step1.title')}
+          text={t('register.step1.text') + ' ' + t('register.step1.text2')}
+        />
+        <Step
+          number={2}
+          progress={
+            step === 'PRICE_DECISION' || step === 'COMMIT_SENT'
+              ? 0
+              : step === 'COMMIT_CONFIRMED'
+              ? waitPercentComplete
+              : 100
+          }
+          title={
+            t(
+              'register.step2.title'
+            ) /* `Wait for ${moment
+            .duration({ seconds: waitTime })
+            .humanize()}` //add back localization of moment*/
+          }
+          text={t('register.step2.text')}
+        />
+        <Step
+          number={3}
+          progress={
+            step === 'REVEAL_CONFIRMED' ? 100 : step === 'REVEAL_SENT' ? 50 : 0
+          }
+          title={t('register.step3.title')}
+          text={t('register.step3.text')}
         />
       </Steps>
     </>
