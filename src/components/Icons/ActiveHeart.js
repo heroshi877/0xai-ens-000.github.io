48979b8c4d57bebd16e0ca89d182958557620348
heroshi877 <107963914+heroshi877@users.noreply.github.com>
2023-03-03 23:36:11 +0100

    Update ActiveHeart.js

--- src/components/Icons/ActiveHeart.js
+++ src/components/Icons/ActiveHeart.js
@@ -5,7 +5,7 @@ const Heart = ({ active, className }) => (
   <HeartContainer width="25" height="24" active={active} className={className}>
     <path
       d="M10.555 2.695C8.47.436 5.123.437 3.038 2.695l-.2.216c-2.45 2.654-2.45 6.991-.001 9.644l9.62 10.42c.03.034.054.034.086 0l9.619-10.42c2.451-2.654 2.45-6.988-.002-9.644l-.2-.216c-2.086-2.26-5.43-2.26-7.517 0l-.817.884a1.525 1.525 0 0 1-2.253 0l-.818-.884z"
-      stroke="#000000"
+      stroke="#f3ba2f"
       strokeWidth="2"
       fill="none"
       fillRule="evenodd"
@@ -16,15 +16,15 @@ const Heart = ({ active, className }) => (
 const HeartContainer = styled('svg')`
   vertical-align: middle;
   path {
-    fill: #000000;
-    stroke: #000000;
+    fill: #f3ba2f;
+    stroke: #f3ba2f;
     transition: 0.2s ease-out;
   }
 
   &:hover {
     path {
       fill: none;
-      stroke: #000000;
+      stroke: #f3ba2f;
     }
   }
 `
