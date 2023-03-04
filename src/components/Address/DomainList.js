5f4936713929274e6e9c5c83d98a0c8154e26a99
heroshi877 <107963914+heroshi877@users.noreply.github.com>
2023-03-04 03:42:22 +0100

    Update DomainList.js

--- src/components/Address/DomainList.js
+++ src/components/Address/DomainList.js
@@ -13,6 +13,8 @@ const NoDomainsContainer = styled('div')`
   box-shadow: 3px 4px 6px 0 rgba(229, 236, 241, 0.3);
   border-radius: 40px;
   margin-bottom: 40px;
+  margin-left: 15px;
+  margin-right: 15px;
 
   h2 {
     color: #f3ba2f;
