0a3bf00d88ab9dc5ee369d6040109ad9357c1a5a
heroshi877 <107963914+heroshi877@users.noreply.github.com>
2023-03-11 19:45:22 +0100

    Update Tabs.js

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -3,8 +3,8 @@ import styled from '@emotion/styled/macro'
 
 export const Tab = styled('div')`
   font-size: 14px;
-  background: ${({ active }) => (active ? '#f3ba2f' : 'transparent')};
-  color: ${({ active }) => (active ? 'black' : '#f3ba2f')};
+  background: ${({ active }) => (active ? '#ffffff' : 'transparent')};
+  color: ${({ active }) => (active ? 'black' : '#ffffff')};
   transform: scale(${({ active }) => (active ? '1.02' : '1')});
   transition: background 0.1s ease-out, transform 0.3s ease-out;
   padding: 10px 30px;
@@ -12,14 +12,14 @@ export const Tab = styled('div')`
   &:hover,
   &:visited {
     cursor: pointer;
-    color: ${({ active }) => (active ? 'black' : '#f3ba2f')};
+    color: ${({ active }) => (active ? 'black' : '#ffffff')};
   }
 `
 
 export const TabsContainer = styled('div')`
   display: inline-flex;
   justify-content: flex-start;
-  border: 1px solid #f3ba2f;
+  border: 1px solid #ffffff;
   border-radius: 90px;
 `
 
