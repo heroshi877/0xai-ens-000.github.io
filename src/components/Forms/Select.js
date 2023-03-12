5475049e13ea8b36e2d49e78bda8178a0982260c
heroshi877 <107963914+heroshi877@users.noreply.github.com>
2023-03-12 02:41:32 +0100

    Update Select.js

--- src/components/Forms/Select.js
+++ src/components/Forms/Select.js
@@ -14,7 +14,7 @@ const styles = {
     textTransform: 'lowercase',
     fontWeight: '700',
     fontSize: '12px',
-    color: '#745600',
+    color: '#818181',
     letterSpacing: '0.5px'
   }),
   option: (styles, { data, isDisabled, isFocused, isSelected }) => {
