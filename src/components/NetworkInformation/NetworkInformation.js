b4d60c9844d0f47feb69565e735fff9673873828
heroshi877 <107963914+heroshi877@users.noreply.github.com>
2023-03-14 16:47:20 +0100

    Update NetworkInformation.js

--- src/components/NetworkInformation/NetworkInformation.js
+++ src/components/NetworkInformation/NetworkInformation.js
@@ -84,6 +84,8 @@ const AccountContainer = styled('div')`
   ${mq.medium`
     transform: translate(-25px, 5px);
     width: 225px;
+    background: #5e5e5e;
+    border-radius: 50px;
     &:hover {
       background: white;
       box-shadow: -4px 18px 70px 0 rgba(108, 143, 167, 0.32);
